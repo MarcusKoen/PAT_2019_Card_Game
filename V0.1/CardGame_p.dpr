10948c9fb5d6ec3e67e6a842042940c035717504
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2019-04-09 18:31:44 +0200

    DB componenets added

--- V0.1/CardGame_p.dpr
+++ V0.1/CardGame_p.dpr
@@ -2,13 +2,15 @@ program CardGame_p;
 
 uses
   Vcl.Forms,
-  CGLogIn_u in 'CGLogIn_u.pas' {CGLogIn};
+  CGLogIn_u in 'CGLogIn_u.pas' {Login},
+  CGServer_u in 'CGServer_u.pas' {frmServer};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TCGLogIn, CGLogIn);
+  Application.CreateForm(TLogin, Login);
+  Application.CreateForm(TfrmServer, frmServer);
   Application.Run;
 end.
